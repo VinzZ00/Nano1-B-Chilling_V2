b6834e18ffd8083af149ecef33dc10fd1f17cf6f
Vinz <elvin.ri39@gmail.com>
2023-12-26 22:15:32 +0700

    UI done but Still dont have the recognizer

--- B'Chilling/HomePage/Components/CustomFormCell.swift
+++ B'Chilling/HomePage/Components/CustomFormCell.swift
@@ -12,37 +12,47 @@ class CustomFormCell: UIView {
     var name : String = ""
     var emotions : Emotions = .chill
     
-    var nameField : UITextField {
+    var nameField : UITextField = {
         let textField = UITextField()
-        
+        textField.backgroundColor = .white
+        textField.clipsToBounds = true
+        textField.layer.borderColor = UIColor.black.cgColor
+        textField.layer.borderWidth = 2
         textField.borderStyle = .roundedRect
+        textField.layer.cornerRadius = 19
+        
         textField.placeholder = "Enter text"
         return textField
-    }
+    }()
     
-    var chillEmoticon : UILabel {
+    var chillEmoticon : UILabel = {
         let l = UILabel()
         l.textAlignment = .center
         l.font = UIFont.systemFont(ofSize: 24)
         l.text = "🥶"
+        
+        let tapGest = UITapGestureRecognizer(target: self, action: #selector(handleTapGest(_:)))
+        l.addGestureRecognizer(tapGest)
         return l
-    }
+    }()
     
-    var blueEmoticon : UILabel {
+    var blueEmoticon : UILabel = {
         let l = UILabel()
         l.textAlignment = .center
         l.font = UIFont.systemFont(ofSize: 24)
         l.text = "😕"
+        l.isUserInteractionEnabled = true
         return l
-    }
+    }()
     
-    var stressEmoticon : UILabel {
+    var stressEmoticon : UILabel = {
         let l = UILabel()
         l.textAlignment = .center
         l.font = UIFont.systemFont(ofSize: 24)
         l.text = "🤯"
+        l.isUserInteractionEnabled = true
         return l
-    }
+    }()
     
     override init(frame: CGRect) {
         super.init(frame: frame)
@@ -55,26 +65,46 @@ class CustomFormCell: UIView {
     }
     
     @objc func handleTapGest(_ sender : UITapGestureRecognizer) {
-        if let label = sender.view as? UILabel
-        {
-            switch label.text! {
-            case "🥶":
-                emotions = Emotions(rawValue: label.text!)!
-                blueEmoticon.gestureRecognizers?[0].isEnabled = false
-                stressEmoticon.gestureRecognizers?[0].isEnabled = false
-            case "😕":
-                emotions = Emotions(rawValue: label.text!)!
-                chillEmoticon.gestureRecognizers?[0].isEnabled = false
-                stressEmoticon.gestureRecognizers?[0].isEnabled = false
-            default :
-                emotions = Emotions(rawValue: label.text!)!
-                chillEmoticon.gestureRecognizers?[0].isEnabled = false
-                blueEmoticon.gestureRecognizers?[0].isEnabled = false
-            }
-        }
+//        if let label = sender.view as? UILabel
+//        {
+//            switch label.text! {
+//            case "🥶":
+//                print("Chill")
+//                emotions = Emotions(rawValue: label.text!)!
+//                blueEmoticon.gestureRecognizers?[0].isEnabled = false
+//                stressEmoticon.gestureRecognizers?[0].isEnabled = false
+//            case "😕":
+//                print("blue")
+//                emotions = Emotions(rawValue: label.text!)!
+//                chillEmoticon.gestureRecognizers?[0].isEnabled = false
+//                stressEmoticon.gestureRecognizers?[0].isEnabled = false
+//            default :
+//                print("default")
+//                emotions = Emotions(rawValue: label.text!)!
+//                chillEmoticon.gestureRecognizers?[0].isEnabled = false
+//                blueEmoticon.gestureRecognizers?[0].isEnabled = false
+//            }
+//        }
+        
+        print("tapped")
     }
     
     func setup() {
+        // Remove Auto Layout
+        
+        
+        // Adding HandleTap to Button
+        let tapGest = UITapGestureRecognizer(target: self, action: #selector(handleTapGest(_:)))
+        
+        chillEmoticon.addGestureRecognizer(tapGest)
+//        stressEmoticon.addGestureRecognizer(tapGest)
+//        blueEmoticon.addGestureRecognizer(tapGest)
+
+        chillEmoticon.isUserInteractionEnabled = true
+//        stressEmoticon.isUserInteractionEnabled = true
+//        blueEmoticon.isUserInteractionEnabled = true
+        
+        self.isUserInteractionEnabled = true
         
         // Adding all into UIView
         addSubview(nameField)
@@ -82,60 +112,34 @@ class CustomFormCell: UIView {
         addSubview(blueEmoticon)
         addSubview(stressEmoticon)
         
-        // Remove Auto Layout
         self.subviews.forEach { v in
             v.translatesAutoresizingMaskIntoConstraints = false
         }
         
-        // Adding HandleTap to Button
-//        let tapGest = UITapGestureRecognizer(target: self, action: #selector(handleTapGest))
-//        
-//        chillEmoticon.addGestureRecognizer(tapGest)
-//        stressEmoticon.addGestureRecognizer(tapGest)
-//        blueEmoticon.addGestureRecognizer(tapGest)
-        
         // Setting Layout
         let safeAreaGuide = self.safeAreaLayoutGuide
         
-//        NSLayoutConstraint.activate([
-//            // Name Field
-////            nameField.topAnchor.constraint(equalTo: self.topAnchor),
-////            nameField.leftAnchor.constraint(equalTo: self.leftAnchor),
-//            nameField.widthAnchor.constraint(equalToConstant: 228),
-//            nameField.heightAnchor.constraint(equalToConstant: 34.73),
-//            
-//            //Emoticons
-//            chillEmoticon.topAnchor.constraint(equalTo: nameField.bottomAnchor, constant: 11.27),
-//            chillEmoticon.leftAnchor.constraint(equalTo: safeAreaGuide.leftAnchor),
-//            
-//            blueEmoticon.topAnchor.constraint(equalTo: nameField.bottomAnchor, constant: 11.27),
-//            blueEmoticon.leftAnchor.constraint(equalTo: chillEmoticon.rightAnchor, constant: 36),
-//            
-//            stressEmoticon.topAnchor.constraint(equalTo: nameField.bottomAnchor, constant: 11.27),
-//            stressEmoticon.rightAnchor.constraint(equalTo: safeAreaGuide.rightAnchor)
-//        
-//        ])
-        
-        let constraints = [
-                // Name Field
-//                nameField.topAnchor.constraint(equalTo: self.topAnchor),
-//                nameField.leftAnchor.constraint(equalTo: self.leftAnchor),
-                nameField.widthAnchor.constraint(equalToConstant: 228),
-                nameField.heightAnchor.constraint(equalToConstant: 34.73),
-                
-                // Emoticons
-                chillEmoticon.topAnchor.constraint(equalTo: nameField.bottomAnchor, constant: 11.27),
-                chillEmoticon.leftAnchor.constraint(equalTo: safeAreaGuide.leftAnchor),
-                
-                blueEmoticon.topAnchor.constraint(equalTo: nameField.bottomAnchor, constant: 11.27),
-                blueEmoticon.leftAnchor.constraint(equalTo: chillEmoticon.rightAnchor, constant: 36),
-                
-                stressEmoticon.topAnchor.constraint(equalTo: nameField.bottomAnchor, constant: 11.27),
-                stressEmoticon.rightAnchor.constraint(equalTo: safeAreaGuide.rightAnchor)
-            ]
+        NSLayoutConstraint.activate([
+            // Name Field
+            nameField.topAnchor.constraint(equalTo: self.topAnchor),
+            nameField.leadingAnchor.constraint(equalTo: self.leadingAnchor),
+            nameField.widthAnchor.constraint(equalToConstant: 228),
+            nameField.heightAnchor.constraint(equalToConstant: 34.73),
             
-            // Activate constraints
-            NSLayoutConstraint.activate(constraints)
+            //Emoticons
+            chillEmoticon.topAnchor.constraint(equalTo: nameField.bottomAnchor, constant: 11.27),
+            chillEmoticon.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 30),
+            chillEmoticon.heightAnchor.constraint(equalToConstant: 200),
+//
+            blueEmoticon.topAnchor.constraint(equalTo: nameField.bottomAnchor, constant: 11.27),
+            blueEmoticon.leadingAnchor.constraint(equalTo: chillEmoticon.trailingAnchor, constant: 36),
+//
+            stressEmoticon.topAnchor.constraint(equalTo: nameField.bottomAnchor, constant: 11.27),
+            stressEmoticon.leadingAnchor.constraint(equalTo: blueEmoticon.trailingAnchor, constant: 36),
+            
+        ])
+        
+        
         
     }
 }
--- B'Chilling/HomePage/View/HomeViewController.swift
+++ B'Chilling/HomePage/View/HomeViewController.swift
@@ -18,21 +18,28 @@ class HomeViewController: UIViewController {
     var commonColor : UIColor = UIColor(red: 20/255, green: 202/255, blue: 225/255, alpha: 1)
     var presenter : HomePresenterProtocol?
     
-    var users : [CustomFormCell] {
+    var users : [CustomFormCell] = {
 //        CGRect(x: 0, y: 0, width: 228, height: 96)
         let firstPersonForm : CustomFormCell = CustomFormCell()
         let secondPersonForm : CustomFormCell = CustomFormCell()
         let thirdPersonForm : CustomFormCell = CustomFormCell()
         return [firstPersonForm, secondPersonForm, thirdPersonForm]
+    }()
+    
+    @objc func tapLogo() {
+        print("Logo Had been tapped")
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        
+        let tapgest = UITapGestureRecognizer(target: self, action: #selector(tapLogo))
         
         view.backgroundColor = .white
         imageView = UIImageView(image: LogoImage)
+        imageView.addGestureRecognizer(tapgest)
+        
+        
         
         innerView = UIView()
         innerView.layer.cornerRadius = 43
@@ -46,7 +53,7 @@ class HomeViewController: UIViewController {
 //            innerView.addSubview(formCell)
 //        })
         
-        innerView.addSubview(users[0])
+        users[0].isUserInteractionEnabled = true
         
         exploreButton = UIButton(type: .system)
         exploreButton.setTitle("Explore", for: .normal)
@@ -56,10 +63,10 @@ class HomeViewController: UIViewController {
         self.view.addSubview(imageView)
 //        
         innerView.addSubview(TextDescription)
-
+        innerView.addSubview(users[0])
 //        innerView.addSubview(exploreButton)
 //
-//        
+//
         innerLayoutSetup()
 //        
         self.view.addSubview(innerView)
@@ -71,14 +78,14 @@ class HomeViewController: UIViewController {
         innerView.subviews.forEach { v in
             v.translatesAutoresizingMaskIntoConstraints = false
         }
-//        
+//
         NSLayoutConstraint.activate([
             TextDescription.topAnchor.constraint(equalTo: innerView.topAnchor, constant: 48),
             TextDescription.heightAnchor.constraint(equalToConstant: 58),
             TextDescription.centerXAnchor.constraint(equalTo: innerView.centerXAnchor),
             
-            
-            users[0].topAnchor.constraint(equalTo: TextDescription.bottomAnchor, constant: 33),
+            users[0].leadingAnchor.constraint(equalTo: innerView.leadingAnchor, constant: 20)
+//            users[0].topAnchor.constraint(equalTo: TextDescription.bottomAnchor, constant: 33),
 //
 //            users[1].topAnchor.constraint(equalTo: users[0].topAnchor, constant: 11),
 //            
